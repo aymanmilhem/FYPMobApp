2415cdc024aba0d772d959528771a95a7d35332d
Ayman Milhem <aymanmilhem@gmail.com>
2019-12-02 18:34:50 +1300

    commit everything else - this time .vs did not show up in the ubtracked folders list

---
+++ GreenWorld/GreenWorld/MainMenuPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreenWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMenuPage : ContentPage
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void MainPageLoginButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+
+        private async void MainPageProfileButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProfilePageNew());
+        }
+
+        private async void MainPageRecordTasksButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RecordTasksPage());
+        }
+
+        private async void MainPageBrowseAsGuestButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void MainPageSignUpButton_OnClicked(object sender, EventArgs e)
+        {
+            // change later to the right navigation page
+            await Navigation.PushAsync(new SignUp());
+        }
+
+        private async void MainPageUserListButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ClientList(), true);
+        }
+    }
+}
---
+++ GreenWorld/GreenWorld/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace GreenWorld
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(40)]
+        public string FirstName { get; set; }
+        
+        [MaxLength(40)]
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public int PointsTotal { get; set; }
+        public int[] CompletedTasksIds { get; set; }
+
+        public string ProfileImageUrl { get; set; }
+
+    }
+}
